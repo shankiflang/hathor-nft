62986f475d3b6a0603ad4ca3b8e35d12f759dc4c
Benjamin BENOIT <shankiflang@gmail.com>
2021-10-23 14:58:27 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+const htrWallet = ""
+const nftName = ""
+const collectionName = ""
+
+
+// Connectoin a ipfs
+// Go to nftName -> collectionName
+// Get all folders
+// Loop all folders et get CID of json
+// For each json, push nft to htrWallet
